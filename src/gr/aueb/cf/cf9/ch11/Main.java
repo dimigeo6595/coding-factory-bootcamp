6de4cb195549a71bbaca53ce510d011d46bf0e33
GiorgosDimitriadis <dimigeo6595@gmail.com>
2025-12-03 21:00:49 +0200

    feat: add immutable class

--- src/gr/aueb/cf/cf9/ch11/Main.java
+++ src/gr/aueb/cf/cf9/ch11/Main.java
@@ -4,6 +4,28 @@ public class Main {
     public static void main(String[] args) {
 
 
+
+       // Customer customer = new Customer(1L.....);
+       Customer customer1 = new Customer();
+
+
+
+
+
+        Product milk = new Product(1L, "Milk A", "Light Milk", 1.5, 2, true);
+        Product juice = new Product(2L, "Juice A", "Orange Juice", 2.5, 1, false);
+
+
+
+
+
+
+
+
+
+
+
+
     Student student = new Student();
     Student alice = new Student();
     Student bob = new Student(3, "student", "student");

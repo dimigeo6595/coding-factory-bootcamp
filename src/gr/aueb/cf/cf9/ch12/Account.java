edb0d2bced4c20b6306ee6b9e9fa3455e72296ad
GiorgosDimitriadis <dimigeo6595@gmail.com>
2025-12-08 19:12:28 +0200

    feat: add public API methods

--- src/gr/aueb/cf/cf9/ch12/Account.java
+++ src/gr/aueb/cf/cf9/ch12/Account.java
@@ -1,5 +1,7 @@
 package gr.aueb.cf.cf9.ch12;
 
+import java.lang.invoke.LambdaMetafactory;
+
 public class Account {
     private long id;
     private String iban;
@@ -67,5 +69,53 @@ public class Account {
 
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    //Public API
+
+    /**
+     * Deposit the specified amount of money into the account.
+     * @param amount the amount to deposit
+     * @throws Exception id the amount is negative
+     */
+    public void deposit (double amount) throws Exception {
+        try {
+            if (amount < 0) throw new Exception("The amount should be positive");
+            balance += amount;
+            // log
+        } catch (Exception e) {
+            System.err.println("Negative amount= " + amount + "is not allowed." + "\n" + e.getMessage());
+            throw e;
+        }
+    }
+
+    /**
+     * Withdraws the specified amount of money.
+     * @param amount The amount to be withdrawn.
+     * @param ssn The SSN of the account holder.
+     * @throws Exception If the amount is negative, the balance insufficient or the SSN does not match.
+     */
+    public void withdraw(double amount, String ssn) throws Exception{
+        try {
+            if (amount < 0) throw new Exception("The amount should be positive");
+            if (amount > balance) throw new Exception("Insufficient balance.");
+            if (!ssn.equals(this.ssn)) throw new Exception("The SSN does not match.");
+            balance -= amount;
+        }catch (Exception e){
+            System.err.println("Withdraw failed:" + "\n" + e.getMessage());
+        }
+    }
+
+    /**
+     * Returns the current balance of this account.
+     * @return the current balance of this account
+     */
+    public double getAccountBalance(){
+        return getBalance();
+    }
+
+    public String accountToString(){
+        return "Account ID: " + id + " IBAN: " + " Firstname: " + firstname +
+                " Lastname: " + lastname + " SSN: " + ssn + " Balance: ";
     }
 }

eec89f3d6101865e7e1d10ba78a943d569a0ba78
GiorgosDimitriadis <dimigeo6595@gmail.com>
2025-12-14 15:54:45 +0200

    feat: add DI / IoC

---
+++ src/gr/aueb/cf/cf9/ch15/marker/Product.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.cf9.ch15.marker;
+
+import java.security.PublicKey;
+
+public class Product {
+    private int id;
+    private String title;
+
+    public Product(){
+
+    }
+
+    public Product(int id, String title){
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

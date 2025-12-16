dfa8f10227e2d2ea2c502a35ca3b7cbf7e9a42ed
GiorgosDimitriadis <dimigeo6595@gmail.com>
2025-12-16 20:53:35 +0200

    feat: add hashcode equals override

---
+++ src/gr/aueb/cf/cf9/ch16/equals_hashcode/Point.java
@@ -0,0 +1,52 @@
+package gr.aueb.cf.cf9.ch16.equals_hashcode;
+
+import java.util.Objects;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(){
+
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Point point)) return false;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}

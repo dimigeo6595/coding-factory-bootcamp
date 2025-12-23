7fe98aab1b83328330cdf5429875e21d960a5c1c
GiorgosDimitriadis <dimigeo6595@gmail.com>
2025-12-23 18:52:51 +0200

    feat: add enums examples

--- src/gr/aueb/cf/cf9/ch17/sorting/Main.java
+++ src/gr/aueb/cf/cf9/ch17/sorting/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class Main {
+public class  Main {
 
     public static void main(String[] args) {
         List<Product> products = new ArrayList<>(List.of(

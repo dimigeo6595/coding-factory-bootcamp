05bada060fbf1be17717e1e72344879ed4505aa1
GiorgosDimitriadis <dimigeo6595@gmail.com>
2025-12-23 14:11:06 +0200

    feat: add streams example with collections

---
+++ src/gr/aueb/cf/cf9/ch17/streams/ReduceMain.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.cf9.ch17.streams;
+
+public class ReduceMain {
+}
+package gr.aueb.cf.cf9.ch17.streams;
+
+import gr.aueb.cf.cf9.ch17.sorting.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+

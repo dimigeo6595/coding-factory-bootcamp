4c997e6271f875e7e87d1138e4bf85ba474dfeda
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-01-13 18:52:05 +0200

    feat: add greedy pattern

---
+++ src/gr/aueb/cf/cf9/ch19/GreedyPattern.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.cf9.ch19;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Greedy pattern.
+ */
+public class GreedyPattern {
+
+
+    public static void main(String[] args) {
+        String str = "login;alice;pass;12345;";
+
+        Pattern pattern = Pattern.compile(".*;");
+        Matcher matcher = pattern.matcher(str);
+
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
